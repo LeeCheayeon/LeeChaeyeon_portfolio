346438645164b173dd65136f4bf278ace86570f6
leechaeyeon <yeontachi@gmail.com>
2022-06-21 15:30:33 +0900

    보기편하게 정리해봤다.

--- Portfoilo/javascript/module/3th.js
+++ Portfoilo/javascript/module/3th.js
@@ -1,3 +1,4 @@
+import buttonMaker from "./button.js";
 import divMaker from "./divMaker.js";
 import boxstyle from "./function.js";
 
@@ -7,9 +8,7 @@ function thrMaker(parentTag){
   const projectOne = document.getElementById('projectOne');
   boxstyle(projectOne, "100vw", '100vh', "#ff6666");
   // 나가기 버튼
-  divMaker(projectOne, "div", "ESC");
-  const ESC = document.getElementById("ESC");
-  boxstyle(ESC, "10vw", "10vw", "#333");
+  buttonMaker(projectOne);
 }
 
 export default thrMaker;
