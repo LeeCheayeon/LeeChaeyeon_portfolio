aa442109cfeb38af99117dea9e80d843429ff18c
leechaeyeon <yeontachi@gmail.com>
2022-06-22 18:02:16 +0900

    일단 page-2 div 간단하게 작성중

--- Portfoilo/javascript/module/contents.js
+++ Portfoilo/javascript/module/contents.js
@@ -2,7 +2,18 @@ import { contentArr } from "./data.js";
 import pageMaker from "./pageMaker.js";
 
 function contentsMaker(parentTag){
-  parentTag.innerHTML = pageMaker(contentArr, "div", "#333");
+  parentTag.innerHTML = `
+  <div id="contents">
+  ${pageMaker(contentArr, "div", "#333")}
+  </div>
+  `
+
+  const contents = Array.from(document.querySelectorAll("#contents>div"));
+  contents.forEach((value)=>{
+    // 목차 박스 카드용으로 간단하게 div 지정 
+  });
+  console.log(contents);
+  // const co
 }
 
 export default contentsMaker;
