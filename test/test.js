6d344963eedf1776436f07681935f4c31c8cd17e
leechaeyeon <yeontachi@gmail.com>
2022-06-22 12:03:53 +0900

    일단 한번 해보았는데... 캡쳐링이나 버블링으로 해결을 했는지 모르겠다.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 
 const pageArr = [1,2,3,4,5,6];
 const pageData = ["One", "Two", "Three", "Four", "Five", "Six"]
+const buttonData = ["Google", "T", "esc"]
 const colorArr = ["#333", "#d4d4d4", "#ff6666", "#333", "#d4d4d4", "#ff6666"]
 const contentArr = pageData.slice(2,6);
 console.log(contentArr);
@@ -67,18 +68,33 @@ window.addEventListener("load", function(){
     });
     if(elem.id === "Two"){
       //두번째 페이지에서 여러 요소가 만들어져야하므로 추가로 넣어둠
-      event.target.style.backgroundColor = "#333";
-      console.log(event.target.innerHTML);
-      // event.target.innerHTML = "";
+      // event.target.style.backgroundColor = "#333";
+      // console.log(event.target.innerHTML);
       event.target.innerHTML = pageMaker(contentArr, "div","#333");
+      // 2번째 페이지에 3~마지막 페이지 버튼이 있어야해서 생성함
+      const contentsBox = document.querySelector("#Two");
+      // 2페이지를 식별
+      // console.log(contentsBox);
+      contentsBox.addEventListener("click", function(event){
+        // 2페이지에 클릭이벤트
+        console.log(event.target.id);
+        for(let i=0; i<pageData.length; i++){
+          console.log(pages[i].id);
+          if(event.target.id === pages[i].id){
+            // 만약 이벤트 타겟의 아이디와 페이지들 i번째의 id와 같다면
+              console.log("dlTek");
+              pages[i].style.display = "block";
+              // 페이지i번째의 디스플레이를 "block"으로 바꿔줘
+              pages[i].innerHTML = pageMaker(buttonData, 'div', '#ff6666');
+            }
+          }
+        });
+      }else if(event.target.id === "esc"){
+        event.target.style.display = "none";
+        console.dir(event.target);
+        console.log(pages[1].style.display);
+        pages[1].style.display = "block";
+      }
     }
-    // 
-    if(event.target.id === elem.id){
-    console.log(elem.id);
-    console.log(event.target.id);
-      event.target.style.display = "block";
-      // 디스플레이를 "none"이 아니라 00페이지로 이동하는 연결을 해야한다. 
-    }
-  }
+  });
 });
-})
