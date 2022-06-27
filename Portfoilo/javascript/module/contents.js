118a3642f83cefb39595158a6a1545dcfd77c3b4
leechaeyeon <yeontachi@gmail.com>
2022-06-27 13:10:29 +0900

    슬라이더 적용..

--- Portfoilo/javascript/module/contents.js
+++ Portfoilo/javascript/module/contents.js
@@ -14,7 +14,7 @@ function contentsMaker(parentTag,target,targetP, targetPS){
 
   const content = document.getElementById("contents");
   // boxstyle(content, "100vw", "50vh", "none");
-  // displayStyle(content, "inlineFlex","center", "flexEnd", "column");
+  displayStyle(content, "flex","center", "flexEnd", "row");
   // 왜 여기서 align-items: flex-end;가 안먹히는걸까?
   const contents = Array.from(document.querySelectorAll("#contents>div"));
   contents.forEach((value, index)=>{
